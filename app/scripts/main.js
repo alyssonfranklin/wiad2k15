d7bc86cdeb2d87981071082c6e92cc4f20fdb884
Alysson Franklin Martins Moreira <alysson.franklin@gmail.com>
2015-01-21 05:10:35 -0200

    fixes & improvements for #11 and #18

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -22,8 +22,6 @@
   var querySelector = document.querySelector.bind(document);
   var querySelectorAll = document.querySelectorAll.bind(document);
 
-  var stickyContainer = document.getElementById("sticky");
-  var fluidContainer = document.getElementById("fluid");
   var navdrawerContainer = querySelector('.navdrawer-container');
   var speakDetailsContainer = querySelectorAll('.tracks-about');
   var body = document.body;
@@ -73,41 +71,6 @@
     }
   }
 */
-  //gMaps
-      function initialize() {
-        var myLatlng = new google.maps.LatLng(19.372090, -99.261421);
-        var mapOptions = {
-          center: myLatlng,
-          zoom: 17,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        
-        var map = new google.maps.Map(document.getElementById("map_canvas"),
-            mapOptions);
-        
-        var contentString = '<div id="content" style="width:300px; height:280px;">'+
-        '<h3 style="white-space:nowrap;">WIAD CDMX 2015</h3><h4>IBM de México<br /><small>S. de R.L de C.V</small></h4>'+
-        'Alfonso Nápoles Gándara No. 3111<br />Col. Parque Corporativo de Peña Blanca<br />Delegación Alvaro Obregón<br />C.P. 01210'+
-        '</div>';
-
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString
-        });
-
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map,
-            title: 'WIAD CDMX 2015'
-        });
-
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.open(map,marker);
-        });
-
-        google.maps.event.addDomListener(window, 'load', initialize);
-
-      }
-
 
   main.addEventListener('click', closeMenu);
   menuBtn.addEventListener('click', toggleMenu);
